ddf0cac22f7b9097e501b8066fcf6d93b6dadd0c
hashirshoaeb <hashirshoaeb@gmail.com>
2021-02-07 16:42:23 +0500

    ipad visual fix

--- src/components/MainBody/MainBody.jsx
+++ src/components/MainBody/MainBody.jsx
@@ -7,27 +7,27 @@ const MainBody = () => {
     <Container>
       <Row className="section-lg pt-4">
         <Col
-          lg="6"
+          md="6"
           className="text-center my-4 d-flex flex-column justify-content-center"
         >
           <h1 className="display-4">{mainBody.title}</h1>
-          <p className="lead">{mainBody.description}</p>        
+          <p className="lead">{mainBody.description}</p>
           {
-          mainBody.icons.map(({image, link}, index)=>{
-            return (
+            mainBody.icons.map(({ image, link }, index) => {
+              return (
                 <a href={link} key={index}>
                   <Image
-                  src={image}
-                  alt="Download StarBook on the App Store"
-                  style={{height:"60px", margin:"40px"}}
-                />
+                    src={image}
+                    alt="Download StarBook on the App Store"
+                    style={{ height: "60px", margin: "40px" }}
+                  />
                 </a>
-            );
+              );
             })
-          }      
+          }
         </Col>
-        <Col lg="6" className="">
-          <Image width="350" src={mainBody.bgImage} alt="starbook home screen" fluid={true}/>
+        <Col md="6" className="">
+          <Image width="350" src={mainBody.bgImage} alt="starbook home screen" fluid={true} />
         </Col>
       </Row>
     </Container>
