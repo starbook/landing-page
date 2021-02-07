d182da0c05529d11d403c431ed0bc9bd75293614
Hanzla <59178380+1hanzla100@users.noreply.github.com>
2021-02-07 10:20:59 +0500

    Re Created AppStore Badge

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ import logo from "./assets/images/logo.png";
 export const navBar = {
     logo: logo,
     title: "StarBook",
-    link: "https://apps.apple.com/pk/app/starbook-journal-ai-diary/id1552418289",
+    link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289?itsct=apps_box&itscg=30200",
 }
 
 export const mainBody = {
     title: "StarBook",
     description: " A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
-    link:"https://apps.apple.com/pk/app/starbook-journal-ai-diary/id1552418289",
+    link:"https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289?itsct=apps_box&itscg=30200",
 }
